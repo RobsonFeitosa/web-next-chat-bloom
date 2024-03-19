83a27c267ad8145bb14da4d1cc15bf9e0175dd9f
RobsonFeitosa <robson.gw@hotmail.com>
2024-03-19 18:41:07 -0300

    Feature: create login link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,10 @@
 import MainLayout from '@/components/Layout/Main' 
-import { HomeContainer, HomeDescription, LogoWrapper } from './styles'
 import Image from 'next/image'
+import { FaSignInAlt, FaUserPlus } from "react-icons/fa";
 import Logo from '@/assets/logo.svg'
+
+import { HomeContainer, HomeDescription, LinkAction, LoginWrapper, LogoWrapper } from './styles'
+
 
 export default function Home() {
   return (
@@ -12,8 +15,17 @@ export default function Home() {
         </LogoWrapper>
 
         <HomeDescription>
-          <span>Uma plataforma que permite a troca de mensagens em tempo real entre usuários. Pode ser usado para comunicação pessoal, suporte ao cliente, colaboração em equipe e muito mais. Esses sistemas variam em complexidade e recursos, oferecendo desde chats simples até assistentes virtuais avançados. </span>
+          <span>Uma plataforma que permite  a troca instantânea de mensagens entre usuários, facilitando a comunicação e colaboração em diversas áreas.</span>
         </HomeDescription>
+
+        <LoginWrapper>
+          <LinkAction href="/signin">
+            <FaSignInAlt size={22}/> <span>Login</span>
+          </LinkAction>
+          <LinkAction href="/signup">
+            <FaUserPlus size={22} /> <span>Registro</span>
+          </LinkAction>
+        </LoginWrapper>
       </HomeContainer>
     </MainLayout>
   )
