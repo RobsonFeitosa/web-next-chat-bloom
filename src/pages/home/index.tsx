e25caed0fdaf949bfd6dce41234782ba9edba270
RobsonFeitosa <robson.gw@hotmail.com>
2024-03-19 17:42:02 -0300

    Feature(style): Home page setup

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,20 @@
 import MainLayout from '@/components/Layout/Main' 
+import { HomeContainer, HomeDescription, LogoWrapper } from './styles'
+import Image from 'next/image'
+import Logo from '@/assets/logo.svg'
 
 export default function Home() {
   return (
     <MainLayout>
-      <div> 
-        <div>Init</div>
-      </div>
+      <HomeContainer> 
+        <LogoWrapper>  
+          <Image src={Logo} width={200} height={160} alt='Chat bloom'/>
+        </LogoWrapper>
+
+        <HomeDescription>
+          <span>Uma plataforma que permite a troca de mensagens em tempo real entre usuários. Pode ser usado para comunicação pessoal, suporte ao cliente, colaboração em equipe e muito mais. Esses sistemas variam em complexidade e recursos, oferecendo desde chats simples até assistentes virtuais avançados. </span>
+        </HomeDescription>
+      </HomeContainer>
     </MainLayout>
   )
 }
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,28 @@
+import { styled } from "@stitches/react";
+
+export const HomeContainer = styled('div', {
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  gap: 60,
+
+
+  width: '100%'
+})
+
+export const HomeDescription = styled('div', {
+  color: 'white',
+  textAlign: 'center',
+
+  width: 300,
+
+
+})
+
+export const LogoWrapper = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
