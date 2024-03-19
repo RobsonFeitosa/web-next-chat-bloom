3ed2cb13b3d0e5030bcab76af58eafe9ab7aea4a
RobsonFeitosa <robson.gw@hotmail.com>
2024-03-19 18:41:16 -0300

    Feature: create login link

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,4 +1,5 @@
 import { styled } from "@stitches/react";
+import Link from "next/link";
 
 export const HomeContainer = styled('div', {
   height: '100vh',
@@ -13,16 +14,43 @@ export const HomeContainer = styled('div', {
 })
 
 export const HomeDescription = styled('div', {
-  color: 'white',
   textAlign: 'center',
-
-  width: 300,
-
-
+  
+  width: 600, 
+  
+  span: {
+    color: '#a6a6a6',
+  }
 })
 
 export const LogoWrapper = styled('div', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center'
+})
+
+export const LoginWrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  gap: 20
+})
+
+export const LinkAction = styled(Link, {
+  all: 'unset',
+
+  background: '#A2BD30',
+  padding: '10px 20px',
+  borderRadius: 6,
+  display: 'flex',
+  gap: 10,
+  
+  cursor: 'pointer',
+    
+  svg: {
+    fill: 'black'
+  },
+  
+  span: {
+    color: 'black',    
+  },
 })
