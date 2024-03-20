5ac59ffe001e3880edfc0cd2ea45b2676a0add96
RobsonFeitosa <robson.gw@hotmail.com>
2024-03-20 04:42:31 -0300

    Chore(style): difine uri routes

--- src/utils/urlBuilder/URLs.ts
+++ src/utils/urlBuilder/URLs.ts
@@ -3,8 +3,16 @@ const AUTH_LOGIN = '/login'
 
 const USERS = '/users' 
 
+const ROOMS = '/rooms' 
+
+const CHATS = '/chats' 
+const CHATS_ROOM = '/room' 
+
 export const URLs = {
   USERS,
   AUTH,
-  AUTH_LOGIN
+  AUTH_LOGIN,
+  ROOMS,
+  CHATS,
+  CHATS_ROOM
 }
